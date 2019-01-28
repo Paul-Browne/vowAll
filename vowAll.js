eea178a335d775047f7ffc948d6ae667a33ba0b1
paulbrowne <Paulmatthewbrowne@gmail.com>
2019-01-28 14:10:33 +0200

    Update vowAll.js

--- vowAll.js
+++ vowAll.js
@@ -1,11 +1,23 @@
 function vowAll(arr, callbackAll) {
+  
+  // array store of responses
   var arrResolved = [];
+  
   arr.forEach(function(obj, index){
+    
+    // null "placeholder"
     arrResolved.push(null);
+    
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4) {
+        
+        // replace null with response, 
+        // if response is 404/403 etc, replace null with undefined
+        // optional request callback
         arrResolved[index] = xhr.status == (obj.status || 200) ? obj.callback ? obj.callback(xhr.responseText) : xhr.responseText : undefined;
+        
+        // "Promise.All" callback when all requests have been resolved
         !~arrResolved.indexOf(null) && callbackAll(arrResolved);
       }
     };
