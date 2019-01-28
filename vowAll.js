9258b5d1cd59e992211844434456fd033bc51c36
paulbrowne <Paulmatthewbrowne@gmail.com>
2019-01-28 13:56:29 +0200

    Update vowAll.js

--- vowAll.js
+++ vowAll.js
@@ -1,50 +1,15 @@
 function vowAll(arr, callbackAll) {
-
-  // initialize an object to store the responses
-  var objResolved = {};
-
-  // loop over all the ajax requests
+  var objResolved = [];
   arr.forEach(function(obj, index){
-
-    // Placeholder each request with a null response
-    // until the response is resolved or rejected
-    objResolved[index] = null;
-
+    objResolved.push(null);
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4) {
-        if(xhr.status == obj.status){
-
-          // Allow callbacks for each individual request
-          if(obj.callback){
-            objResolved[index] = obj.callback(xhr.responseText);
-          }else{
-            objResolved[index] = xhr.responseText;
-          }
-        }else{
-
-          // Resolve rejected requests to undefined
-          objResolved[index] = undefined;
-
-        }
-
-        // After each request is resolved
-        // loop over the resolved object store
-        var noNulls = true;
-        for (var key in objResolved){
-          if(objResolved[key] === null){
-            noNulls = false;
-          }else if(Object.keys(objResolved).length-1 == key && noNulls){
-
-            // if all requests are resolved
-            // call the "promise all" callback
-            callbackAll(objResolved);
-          }
-        }
-
+        objResolved[index] = xhr.status == (obj.status || 200) ? obj.callback ? obj.callback(xhr.responseText) : xhr.responseText : undefined;
+        !~objResolved.indexOf(null) && callbackAll(objResolved);
       }
     };
-    xhr.open(obj.method, obj.url, true);
+    xhr.open(obj.method || "GET", obj.url, true);
     for (var key in obj.headers) {
         xhr.setRequestHeader(key, obj.headers[key]);
     }
