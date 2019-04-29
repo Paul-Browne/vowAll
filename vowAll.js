956a47c1387a31ed27de071824d3627e18bb2ebe
paulbrowne <Paulmatthewbrowne@gmail.com>
2019-04-29 14:41:47 +0300

    Update vowAll.js

--- vowAll.js
+++ vowAll.js
@@ -1,30 +1,42 @@
 function vowAll(arr, callbackAll) {
   
-  // array store of responses
-  var arrResolved = [];
+    // object store of responses
+    var objResolved = {};
   
-  arr.forEach(function(obj, index){
-    
-    // null "placeholder"
-    arrResolved.push(null);
-    
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
+    arr.forEach(function(obj, index) {
+      
+        // null "placeholder"
+        objResolved[index] = null;
+      
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+              
+                // replace null with response, 
+                // if response is 404/403 etc, replace null with undefined
+                // optional request callback
+                objResolved[index] = xhr.status == (obj.status || 200) ? (obj.callback ? obj.callback(xhr.responseText) : xhr.responseText) : undefined;
         
-        // replace null with response, 
-        // if response is 404/403 etc, replace null with undefined
-        // optional request callback
-        arrResolved[index] = xhr.status == (obj.status || 200) ? obj.callback ? obj.callback(xhr.responseText) : xhr.responseText : undefined;
-        
-        // "Promise.All" callback when all requests have been resolved
-        !~arrResolved.indexOf(null) && callbackAll(arrResolved);
-      }
-    };
-    xhr.open(obj.method || "GET", obj.url, true);
-    for (var key in obj.headers) {
-        xhr.setRequestHeader(key, obj.headers[key]);
-    }
-    xhr.send(obj.body);
-  });
-};
+                var allResolved = true;
+                
+                // loop through all responses
+                for (var key in objResolved) {
+                    if (objResolved[key] === null) {
+                      
+                        // if any are unresolved, 
+                        // prevent the callbackAll function from firing
+                        allResolved = false;
+                    }
+                }
+              
+                // "Promise.all" callback when all requests have been resolved
+                allResolved && callbackAll(objResolved);
+            }
+        };
+        xhr.open(obj.method || "GET", obj.url, true);
+        for (var key in obj.headers) {
+            xhr.setRequestHeader(key, obj.headers[key]);
+        }
+        xhr.send(obj.body);
+    });
+}
